e9a9775f599c7f91f467e29e4c401f90e02ffa85
sebastiankarel <sebastiankarel.lembcke@googlemail.com>
2019-11-21 16:04:18 +0100

    wip

--- Main.py
+++ Main.py
@@ -2,5 +2,17 @@ from GameStateVisualization import Visualizer
 import numpy as np
 
 gsv = Visualizer()
-gsv.draw_game_state(7, 2, np.zeros(10, dtype=int), np.ones((7, 7, 2)))
+
+num_players = 3
+max_num_cards = 8
+
+player_cards = np.zeros((num_players, max_num_cards, 2), dtype=int)
+player_cards[0][0] = (1, 2)
+player_cards[0][1] = (4, 3)
+player_cards[0][2] = (10, 4)
+
+player_cards[1][0] = (12, 1)
+player_cards[1][1] = (8, 2)
+
+gsv.draw_game_state(num_players, 2, np.zeros(10, dtype=int), player_cards)
 
